f97e5b2c53e5153f992f14b2845384bad7820940
Andru Cherny <acherny@minexsystems.com>
2017-09-15 19:10:59 +0300

    for CI

--- .docker16/linux/endpoint.sh
+++ .docker16/linux/endpoint.sh
@@ -2,7 +2,7 @@
 cd /home/project/
 cd depends
 set +e
-make -j 4 || error=true
+make -j 12 || error=true
 if [ ${error} ]
 then
     exit -i
@@ -12,4 +12,4 @@ make clean
 find . -type f -name '*.o' -delete
 ./autogen.sh
 CONFIG_SITE=$PWD/depends/x86_64-pc-linux-gnu/share/config.site ./configure --prefix=/
-make all -j 4
+make all -j 12
