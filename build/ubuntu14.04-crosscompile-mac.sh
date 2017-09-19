8b88898419ca02e2eea6e6fe076ea59a19ab5735
Andru Cherny <acherny@minexsystems.com>
2017-09-19 16:39:38 +0300

    Fix output files in mac

--- build/ubuntu14.04-crosscompile-mac.sh
+++ build/ubuntu14.04-crosscompile-mac.sh
@@ -4,4 +4,10 @@ docker build -t minex_coin_14_mac minexcoin_docker/.docker/mac/
 docker run --user $(id -u) -v $(pwd)/minexcoin/:/home/project/ minex_coin_14_mac /endpoint.sh
 
 mkdir $(pwd)/minexcoin_docker/out/minex_coin_14_mac
-cp minexcoin/Minexcoin-Core.dmg out/minex_coin_14_mac/
+
+cp minexcoin/MinexWallet.dmg out/minex_coin_14_mac/
+cp minexcoin/src/minexcoin-cli $(pwd)/minexcoin_docker/out/minex_coin_14_mac/
+cp minexcoin/src/minexcoin-tx $(pwd)/minexcoin_docker/out/minex_coin_14_mac/
+cp minexcoin/src/bench/bench_bitcoin $(pwd)/minexcoin_docker/out/minex_coin_14_mac/
+cp minexcoin/src/minexcoind $(pwd)/minexcoin_docker/out/minex_coin_14_mac/
+cp minexcoin/src/qt/minexcoin-qt $(pwd)/minexcoin_docker/out/minex_coin_14_mac/
--- build/ubuntu16.04-crosscompile-mac.sh
+++ build/ubuntu16.04-crosscompile-mac.sh
@@ -4,6 +4,10 @@ docker build -t minex_coin_16_mac minexcoin_docker/.docker16/mac/
 docker run  --user $(id -u) -v $(pwd)/minexcoin/:/home/project/ minex_coin_16_mac /endpoint.sh
 
 mkdir $(pwd)/minexcoin_docker/out/minex_coin_16_mac
-cp minexcoin/Minexcoin-Core.dmg $(pwd)/minexcoin_docker/out/minex_coin_16_mac/
-cp -R minexcoin/Minexcoin-Qt.app $(pwd)/minexcoin_docker/out/minex_coin_16_mac/
 
+cp minexcoin/MinexWallet.dmg out/minex_coin_16_mac/
+cp minexcoin/src/minexcoin-cli $(pwd)/minexcoin_docker/out/minex_coin_16_mac/
+cp minexcoin/src/minexcoin-tx $(pwd)/minexcoin_docker/out/minex_coin_16_mac/
+cp minexcoin/src/bench/bench_bitcoin $(pwd)/minexcoin_docker/out/minex_coin_16_mac/
+cp minexcoin/src/minexcoind $(pwd)/minexcoin_docker/out/minex_coin_16_mac/
+cp minexcoin/src/qt/minexcoin-qt $(pwd)/minexcoin_docker/out/minex_coin_16_mac/
